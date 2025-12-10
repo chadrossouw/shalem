715cf74b2f89b59266f9261b4740231ba212819d
Chad Rossouw <chadrossouw@gmail.com>
2025-12-10 07:30:26 +0200

    feat: Enhance goals panel with user-specific goal filtering and add description column to goals table

--- resources/js/components/Dashboard/Panel/shalem-student-panel-goals-panel.js
+++ resources/js/components/Dashboard/Panel/shalem-student-panel-goals-panel.js
@@ -14,7 +14,10 @@ export class ShalemStudentPanelGoals extends BaseDashboardConsumer(BaseClass(Lit
     connectedCallback() {
         super.connectedCallback();
         ({fields: this.fields, user: this.user, pillars: this.pillars, selectableGoals: this.selectableGoals} = this._dashboard);
-        console.log('Goals Panel', this.pillars, this.selectableGoals);
+        this.userGoals = this.user.user_goals;
+        this.pillar = this.pillars.find(pillar => {
+            return pillar.slug === this.panel;
+        });
         if(!this.selectableGoals||!this.selectableGoals[this.panel]){
             this._getSelectableGoals();
         }
@@ -25,20 +28,22 @@ export class ShalemStudentPanelGoals extends BaseDashboardConsumer(BaseClass(Lit
 
     render() {
         if(!this.goalList || !this.goalList.length){
-            return html`<shalem-loader>Climbing that hill...</shalem-loader>`
+            return html`<div class="margins"><shalem-loader>Climbing that hill...</shalem-loader></div>`;
         }
         console.log('Rendering Goals Panel', this.goalList);
         return html`
-            <h1>Hello from Goals ${this.panel} Panel</h1>
+            <div class="header_with_icon margins">
+                <div class="icon" aria-hidden="true">${unsafeSVG(goalsIcon)}</div>
+                <h1>Set a${this.pillar.name.startsWith('A') || this.pillar.name.startsWith('E') || this.pillar.name.startsWith('I') || this.pillar.name.startsWith('O') || this.pillar.name.startsWith('U') ? 'n' : ''} ${this.pillar.name} goal</h1>
+            </div>
+            <div class="margins">
+            </div>
         `;
     }
 
     async _getSelectableGoals(){
-        let pillar = this.pillars.find(pillar => {
-            return pillar.slug === this.panel;
-        });
         try{
-            const response = await safeFetch(`${this.restUrl}goals/by-pillar?pillar_id=${pillar.id}`, {
+            const response = await safeFetch(`${this.restUrl}goals/by-pillar?pillar_id=${this.pillar.id}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
@@ -48,12 +53,16 @@ export class ShalemStudentPanelGoals extends BaseDashboardConsumer(BaseClass(Lit
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             const data = await response.json();
-            this.goalList = data.goals;
+            this.goalList = data.goals.filter( goal => {
+                return !this.userGoals.some(userGoal => userGoal.goal_id === goal.id);
+            });
+            
             this.selectableGoals = {
                 ...this.selectableGoals,
                 [this.panel]: data.goals
             };
             this._updateContext({selectableGoals: this.selectableGoals});
+            
         }
         catch(error){
             console.error('Error fetching selectable goals:', error);
